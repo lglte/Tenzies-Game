c3eeeef79c35cf52058b76bdb7bd0e669e88abb4
lglte <89487111+lglte@users.noreply.github.com>
2022-07-01 20:07:47 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+import Die from "./Die";
+import {useState, useEffect} from "react";
+
+function App() {
+  const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+    }
+  }, [dice])
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie())
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    if(!tenzies) {
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ?
+            die :
+            generateNewDie()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+          {...die, isHeld: !die.isHeld} :
+          die
+    }))
+  }
+
+  const diceElements = dice.map(die => (
+      <Die
+          key={die.id}
+          value={die.value}
+          isHeld={die.isHeld}
+          holdDice={() => holdDice(die.id)}
+      />
+  ))
+  return (
+    <div className="App">
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all the dice are the same number.
+        You can freeze each dice by clicking on it.</p>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+      <button
+          className="roll-dice"
+          onClick={rollDice}
+      >
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+    </div>
+  );
+}
+
+export default App;
